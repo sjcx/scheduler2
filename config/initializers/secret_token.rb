3cceef17bd8608992271fe517a5c29ac14a333d8
SJC <sarajoyculver@gmail.com>
2013-01-28 11:35:29 -0800

    Added User model with devise and Bootstrap styling

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scheduler2::Application.config.secret_token = 'b94fe019b4a8e575c1d02e7782c051be4ff696616c49481fb0928ca249537b76f197a6e780060e80aafb8822cb613805e20c0d92fb9d7749d96a327d89a3745e'
